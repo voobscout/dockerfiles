7dbfb476d1947cd970f7761c16778c742a74c3e8
voobscout <voobscout@archlinux.info>
2017-05-14 20:10:55 +0200

    wip...

--- debian/softethervpn/runner.sh
+++ debian/softethervpn/runner.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+sysctl -w net.ipv4.ip_forward=1
 modprobe tun
 
+_ingress() {
+    ip addr add 192.168.30.254/24 dev vpn_ingress
+    iptables -t nat -N ingress
+    iptables -t nat -A ingress -p tcp -m tcp --dport 8443 -j DNAT --to-destination 192.168.30.10:443
+    iptables -t nat -A ingress -p tcp -m tcp --dport 8080 -j DNAT --to-destination 192.168.30.10:80
+    iptables -t nat -A ingress -j RETURN
+    iptables -t nat -A PREROUTING -j ingress
+}
+# -j TEE --gateway 192.168.30.10
 # ip tuntap add mode tap br0p0
 
 _mkdirs() {
@@ -12,10 +22,24 @@ _mkdirs() {
     done
 }
 
-_mkdirs
+_server() {
+    curl $SERVER_CONFIG -o /usr/local/vpnserver/vpn_server.config
+    _mkdirs
+    /usr/local/vpnserver/vpnserver start
+}
 
-/usr/local/vpnclient/vpnclient start
+_client() {
+    curl $CLIENT_CONFIG -o /usr/local/vpnclient/public_ip.vpn
+    /usr/local/vpnclient/vpnclient start
+    cd /usr/local/vpnclient
+    /usr/local/vpnclient/vpncmd 127.0.0.1 /client /cmd AccountImport public_ip.vpn
+}
 
-exec /usr/local/vpnserver/vpnserver execsvc
+_start_vpn() {
+    [[ -z "$SERVER_CONFIG" ]] && _server
+    [[ -z "$CLIENT_CONFIG" ]] && _client
+}
 
-exit $?
+_start_vpn
+
+# tail -f /var/log/vpnserver/server_log/*.log
