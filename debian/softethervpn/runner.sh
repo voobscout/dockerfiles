5da7c488ec5888b0193a931732fa6a4df5436f5e
voobscout <voobscout@archlinux.info>
2017-05-16 16:21:24 +0200

    ..

--- debian/softethervpn/runner.sh
+++ debian/softethervpn/runner.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
-sysctl -w net.ipv4.ip_forward=1
-modprobe tun
-
 _ingress() {
-    ip addr add 192.168.30.254/24 dev vpn_ingress
     iptables -t nat -N ingress
-    iptables -t nat -A ingress -p tcp --dport 8443 -j DNAT --to-destination 192.168.30.10:443
-    iptables -t nat -A ingress -p tcp --dport 8080 -j DNAT --to-destination 192.168.30.10:80
+    iptables -t nat -A ingress -p tcp --dport 443 -j DNAT --to-destination 192.168.30.10:443
+    iptables -t nat -A ingress -p tcp --dport 80  -j DNAT --to-destination 192.168.30.10:80
     iptables -t nat -A ingress -j RETURN
     iptables -t nat -I PREROUTING -j ingress
 
+    iptables -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination 192.168.30.254:443
+    # iptables -t nat -I OUTPUT -p tcp --dport 80 -j DNAT --to-destination 192.168.30.254:80
+
+
     iptables -t nat -A PREROUTING  -p tcp                -d $EXT.IP.ADD.RESS --dport 25 -j DNAT --to-destination 10.122.1.25:25
     iptables -t nat -A OUTPUT      -p tcp                -d $EXT.IP.ADD.RESS --dport 25 -j DNAT --to-destination 10.122.1.25:25
+
     iptables -t nat -A POSTROUTING -p tcp -s 10.122.1.25 -d 10.122.1.25      --dport 25 -j SNAT --to 10.122.1.1
 }
 # ip link set promisc on tap_vpn-br
 # -j TEE --gateway 192.168.30.10
 # ip tuntap add mode tap br0p0
+
+_wait4vpn() {
+    server_ready=0
+    while [ $server_ready -lt 1 ]
+    do
+        sleep 3
+        </dev/tcp/127.0.0.1/443 && server_ready=1
+    done
+}
 
 _mkdirs() {
     pref='/usr/local/vpnserver'
@@ -37,13 +47,7 @@ _server() {
 
     _mkdirs
     /usr/local/vpnserver/vpnserver start
-
-    server_ready=0
-    while [ $server_ready -lt 1 ]
-    do
-        sleep 3
-        </dev/tcp/localhost/443 && server_ready=1
-    done
+    _wait4vpn
 
     $vpncmd HubCreate $S_HUB /password
     $vpncmd ServerPasswordSet $S_PASSWD
@@ -63,6 +67,7 @@ _client() {
     done
 
     /usr/local/vpnclient/vpnclient start
+    _wait4vpn
 
     $vpncmd NicCreate $C_NIC
     $vpncmd AccountCreate $C_ACCOUNT \
@@ -73,12 +78,18 @@ _client() {
     $vpncmd AccountPasswordSet $C_ACCOUNT /password:$C_PASSWD /type:standard
     [[ -n "$C_COMPRESS" ]] && $vpncmd AccountCompressEnable $C_ACCOUNT
     $vpncmd AccountConnect $C_ACCOUNT
+
+    ip addr add $C_NIC_IP dev vpn_$C_NIC
+    # ip link set dev vpn_$C_NIC mtu 1450
 }
 
 _start_vpn() {
     [[ -n "$CLIENT_CONFIG" ]] && _client
     [[ -n "$SERVER_CONFIG" ]] && _server
 }
+
+sysctl -w net.ipv4.ip_forward=1 &> /dev/null
+modprobe tun &> /dev/null
 
 _start_vpn
 
