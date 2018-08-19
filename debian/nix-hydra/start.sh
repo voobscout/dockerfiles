d89c4e1f8b7790ce219d1c36e2b7615d6b1ec81e
Adminius <admin@ipc.0.njk.li>
2018-08-19 16:48:14 +0200

    hydra_remotes

--- debian/nix-hydra/start.sh
+++ debian/nix-hydra/start.sh
@@ -3,36 +3,20 @@ set -e
 
 _chk_env() {
     # [[ $HYDRA_DBI && $HYDRA_DATA ]]
-
-    HYDRA_DBI="dbi:Pg:dbname=hydra;host=dbserver.example.org;user=hydra;"
+    # HYDRA_DBI="dbi:Pg:dbname=hydra;host=dbserver.example.org;user=hydra;"
     HYDRA_DATA=/var/lib/hydra
 }
 
-_create_user() {
-    useradd -m -o -s /bin/bash -u $RUN_UID $RUN_USER
-    chown -R $RUN_USER /home/$RUN_USER
+_ssh_keygen() {
+    k_pre="/etc/ssh/ssh_host_"
+    k_suf="_key"
+    k_types="rsa dsa ecdsa ed25519"
+    kg="/usr/bin/ssh-keygen"
+
+    for i in $k_types;do "${kg}" -q -f "${k_pre}${i}${k_suf}" -N '' -t "${i}";done
 }
 
-_run_yadisk() {
-    local cfg="${HOME}/.config/yandex-disk"
-    [[ -f ${cfg} ]] && rm -rf ${cfg} || true
-
-    runuser $RUN_USER -c '
-    expect -c "
-    spawn /usr/bin/yandex-disk setup
-    expect \": \" {send \"n\r\"}
-    expect \"name: \" {send \"${YANDEX_UNAME}\r\"}
-    expect \"sword: \" {send \"${YANDEX_PASSWD}\r\"}
-    expect \": \" {send \"${HOME}/Yandex.Disk\r\"}
-    expect \": \" {send \"y\r\"}
-    "'
-    runuser $RUN_USER -c "/usr/bin/yandex-disk start"
-    runuser $RUN_USER -c 'tail -f ${HOME}/Yandex.Disk/.sync/core.log'
-}
-
-# nix-channel --add http://hydra.nixos.org/jobset/hydra/master/channel/latest
-# nix-channel --update
-# nix-env -i hydra
-# [[ $RUN_USER && $RUN_UID && $YANDEX_UNAME && $YANDEX_PASSWD ]] && _create_user || (echo  '$RUN_USER && $RUN_UID && $YANDEX_UNAME && $YANDEX_PASSWD are required!' && exit 1)
-
+_ssh_keygen
+/usr/sbin/sshd -D &> /dev/stdout &
+# We should totally fail when that dies:
 exec /home/hydra/.nix-profile/bin/nix-daemon
